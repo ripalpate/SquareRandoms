cf6dcbb398a6a71878c98ecaa107e322508efaeb
Ripal Patel <ripal212007@gmail.com>
2019-03-11 22:53:47 -0500

    working

--- RandomSquare/Program.cs
+++ RandomSquare/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RandomSquare
 {
@@ -6,7 +7,25 @@ namespace RandomSquare
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //var listOfNumbers = new List<int>();
+
+            //Random random = new Random();
+            //for (var i = 0; i < 20; i++)
+            //{
+            //    var populateRandomNaumber = random.Next(1, 50);
+            //   // Console.WriteLine(populateRandomNaumber);
+            //    if (!listOfNumbers.Contains(populateRandomNaumber))
+            //    {
+            //        listOfNumbers.Add(populateRandomNaumber);
+            //    }
+            //    Console.WriteLine(listOfNumbers);
+            //}
+
+            var gerenrateNumbers = new Randomize();
+            gerenrateNumbers.PopulateNumbers();
+
+            Console.ReadLine();
         }
     }
 }
---
+++ RandomSquare/Randomize.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomSquare
+{
+    class Randomize
+    {
+        public List<int> ListOfNumbers = new List<int> () ;
+        public List<int> squareNumbers = new List<int>();
+        public void PopulateNumbers()
+        {
+            var randomNumbersString = "";
+            var squareNumbersString = "";
+            var ListofEvenNumbers = "";
+
+            Random random = new Random();
+            for (var i = 0; i < 20; i++)
+            {
+                var populateRandomNaumber = random.Next(1, 50);
+               
+                     ListOfNumbers.Add(populateRandomNaumber);
+                 
+            }
+
+            foreach (var number in ListOfNumbers)
+            {
+                 randomNumbersString += number + ",";
+            }
+
+            foreach (var number in ListOfNumbers)
+            {
+                var square = number * number;
+                squareNumbers.Add(square);
+                squareNumbersString += square + ",";
+            }
+
+            foreach (var squareNumber in squareNumbers)
+            {
+                if (squareNumber % 2 == 0)
+                {
+                    ListofEvenNumbers += squareNumber + ",";
+                }
+            }
+
+            Console.WriteLine($"list of Random numbers: {randomNumbersString.TrimEnd(',')}");
+            Console.WriteLine($"List of Square numbers: {squareNumbersString.TrimEnd(',')}");
+            Console.WriteLine($"List of Even Square Numbers: {ListofEvenNumbers.TrimEnd(',')}");
+        }
+    }
+}
